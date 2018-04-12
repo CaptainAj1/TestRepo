94649e51890efe5e56fbc521e70b1e4ae6466f11
CaptainAj1 <aj9145348@gmail.com>
2018-04-12 12:36:29 +0530

    second commit

--- prime.php
+++ prime.php
@@ -1,7 +1,9 @@
 <?php
     $ncount=0;
-	$num=2;
-while($ncount<1000){
+	$num=2;?>
+<table border="1px solid" align="center">
+	<tr><th>Table of first 1000 prime numbers</th></tr>
+<?php while($ncount<1000){
 	$count=0;
 		for($i=1;$i<=$num;$i++)
 		{
@@ -11,11 +13,12 @@ while($ncount<1000){
 			}
 		}
 	if($count<3)
-			{
+			{?>
 		
-	echo $num." , ";
-	$ncount=$ncount+1;			
+<tr><td align="center"><?php echo $num;?></td></tr>
+	<?php $ncount=$ncount+1;			
 			
 			} $num=$num+1;
    		}
 ?>
+</table>
