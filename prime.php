184ab6cefe11263c114b1d236d711f0b01265a7e
CaptainAj1 <aj9145348@gmail.com>
2018-04-12 14:04:05 +0530

    second commit

--- prime.php
+++ prime.php
@@ -1,9 +1,15 @@
 <?php
+$time_start=(microtime(true)-$_SERVER["REQUEST_TIME_FLOAT"])*1000;
+    
     $ncount=0;
 	$num=2;?>
-<table border="1px solid" align="center">
-	<tr><th>Table of first 1000 prime numbers</th></tr>
-<?php while($ncount<1000){
+
+<div align="center">
+<b>The first 1000 Prime numbers</b><br><br>
+<table border="1px solid" cellpadding="10">
+	<tr><th>Item #</th><th>Prime</th></tr>
+<?php while($ncount<1000)
+{
 	$count=0;
 		for($i=1;$i<=$num;$i++)
 		{
@@ -14,11 +20,18 @@
 		}
 	if($count<3)
 			{?>
-		
-<tr><td align="center"><?php echo $num;?></td></tr>
-	<?php $ncount=$ncount+1;			
-			
-			} $num=$num+1;
-   		}
-?>
+	<?php $ncount=$ncount+1;?>			
+	<tr><td align="center"><?php echo $ncount;?></td>
+	<td align="center"><?php echo $num;?></td></tr>				
+			<?php } 
+	$num=$num+1;
+   		}?>
 </table>
+<?php 
+$time_end=(microtime(true)-$_SERVER["REQUEST_TIME_FLOAT"])*1000;
+$execution_time=($time_end-$time_start);
+echo "<b>Start time: </b>".$time_start." ms"."</br>";
+echo "<b>End time: </b>".$time_end." ms"."</br>";		
+echo "<b>Total execution time: </b>".$execution_time." ms"."</br>";	
+?>
+</div>
