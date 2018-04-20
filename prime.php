5e298a79a889e84c50b776d4c582b87cc46470bb
CaptainAj1 <aj9145348@gmail.com>
2018-04-20 12:44:50 +0530

    second commit

--- prime.php
+++ prime.php
@@ -1,42 +1,46 @@
-<?php 
-$time_start=(microtime(true)-$_SERVER["REQUEST_TIME_FLOAT"])*1000;
-function primeLoop($number) 
-{
-    // Set default to true
-    $prime = true;
-    
-	for ($div = 2; $div < $number; $div++) 
-	{
-		
-		if (($number % $div) === 0) 
+<?php $time_start=(microtime(true)-$_SERVER["REQUEST_TIME_FLOAT"])*1000;
+function primeno($num)
+ {   
+	if($num<2)
+		return false;
+	if($num==2)
+		return true;
+	if(($num%2)==0)
+		return false;
+	$prime=true;
+		for($div=3;$div<$num;$div+=2)
 		{
-            $prime = false;
-            return $prime;
-        }
-    }
-    // Return true or false
-    return $prime;
-}
+			if($num%$div==0)
+			{
+				$prime=false;
+				return $prime;
+			}
+		}
+	 return $prime;
+  } 
 ?>
 
 <div align="center">
 <b>The first 1000 Prime numbers</b><br><br>
 <table border="1px solid" cellpadding="10">
-	<tr><th>Item #</th><th>Prime</th></tr>
-<?php
-	$count=0;
-$i=2;
-while ($count<1000) {
-    if (primeLoop($i)) {
-		$count++;?>			
-	<tr><td align="center"><?php echo $count;?></td>
+<tr><th>Item #</th><th>Prime</th></tr>
+
+ <?php	
+	 $ncount=0;
+     $i=1;
+ while($ncount<1000)
+{
+	if (primeno($i))
+	{
+		$ncount++;?>
+    <tr><td align="center"><?php echo $ncount;?></td>
 	<td align="center"><?php echo $i;?></td></tr>		
    <?php }
     $i++;
 }
-
-?>	
+?>
 </table><br>
+
 <?php 
 $time_end=(microtime(true)-$_SERVER["REQUEST_TIME_FLOAT"])*1000;
 $execution_time=($time_end-$time_start);
