ed72445dfd93a5b0b80038c04a49fe19563bc441
CaptainAj1 <aj9145348@gmail.com>
2018-04-24 14:45:10 +0530

    second commit

--- prime.php
+++ prime.php
@@ -8,7 +8,8 @@ function primeno($num)
 	if(($num%2)==0)
 		return false;
 	$prime=true;
-		for($div=3;$div<$num;$div+=2)
+	 $limit=sqrt($num)+1;
+		for($div=3;$div<$limit;$div+=2)
 		{
 			if($num%$div==0)
 			{
@@ -17,7 +18,27 @@ function primeno($num)
 			}
 		}
 	 return $prime;
-  } 
+  }
+function Isprime($pno)
+{
+if($pno<2)
+		return false;
+	if($pno==2)
+		return true;
+	if(($pno%2)==0)
+		return false;
+	$result=true;
+	 $sqrt=sqrt($pno)+1;
+		for($selected=3;$selected<$sqrt;$selected++)
+		{
+			if($pno%$selected==0)
+			{
+				$result=false;
+				return $result;
+			}
+		}
+	 return $result;	
+}
 ?>
 
 <div align="center">
@@ -25,12 +46,25 @@ function primeno($num)
 <table border="1px solid" cellpadding="10">
 <tr><th>Item #</th><th>Prime</th></tr>
 
- <?php	
+<?php	
 	 $ncount=0;
      $i=1;
+     $element=1;
+	 $foundPrimes= array();
+for($x=0;$x<=100;$x++)
+{
+	if (primeno($element))
+	{
+		array_push($foundPrimes,$element);
+    }
+    $element++;
+}
+	foreach($foundPrimes as $selected){
+	}
+	
  while($ncount<1000)
 {
-	if (primeno($i))
+	if (Isprime($i))
 	{
 		$ncount++;?>
     <tr><td align="center"><?php echo $ncount;?></td>
@@ -38,7 +72,7 @@ function primeno($num)
    <?php }
     $i++;
 }
-?>
+?>	
 </table><br>
 
 <?php 
